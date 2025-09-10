e169f3cc4a32ba3acb16996f1023bca97d5ed30e
Varun-325 <161681663+Varun-325@users.noreply.github.com>
2025-09-10 22:42:40 +0530

    4th commit

--- backend/src/config.js
+++ backend/src/config.js
@@ -5,6 +5,6 @@ export const config = {
   MONGODB_URI: process.env.MONGODB_URI || "",
   PORT: process.env.PORT || 3000,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_ID,
-  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_SECRET,
   JWT_SECRET: process.env.JWT_SECRET,
 };
