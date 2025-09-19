1563bae5dcaab9e64b8e448efc61558ac003d455
Varun-325 <161681663+Varun-325@users.noreply.github.com>
2025-09-19 15:46:19 +0530

    qr

--- frontend/src/Pages/UrlForm.jsx
+++ frontend/src/Pages/UrlForm.jsx
@@ -1,88 +1,80 @@
-import { Button, Container, TextInput } from "@mantine/core";
-
-import React, { useState } from "react";
-
-import Service from "../utils/http";
-
+import { Button, Container, TextInput } from '@mantine/core'
+import React, { useState } from 'react'
+import Service from '../utils/http'
 const obj = new Service();
 
+
 export default function UrlForm(props) {
-  const generateShortUrl = async (data) => {
-    try {
-      let res = await obj.post("s", data);
-      props.setResponse(res);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+   const generateShortUrl = async(data) => {
+       try {
+           let response = await obj.post("s", data);
+           console.log(response);
+           props.setResponse(response);
+       }catch (error) {
+           console.log(error);
+       }
+   }
 
-  const [data, setdata] = useState({
-    originalUrl: "",
-    expiresAt: "",
-    title: "",
-    customUrl: "",
-  });
 
-  //   obj.post("", data.originalUrl);
-  //   props.set;
-  return (
-    <div>
-      {
-        <Container size="xs" my={40}>
-          <h2 style={{ textAlign: "center" }}>URL Shortner Form</h2>
-          <TextInput
-            radius="md"
-            mb={"md"}
-            label="Original Url"
-            withAsterisk
-            description="Enter Your URL"
-            placeholder="Enter Your URL"
-            onChange={(e) => {
-              setdata({ ...data, originalUrl: e.target.value });
-            }}
-          />
-          <TextInput
-            radius="md"
-            mb={"md"}
-            label="Custom Url(Optional)"
-            description="Enter Custom URL"
-            placeholder="Enter Your URL"
-            onChange={(e) => {
-              setdata({ ...data, customUrl: e.target.value });
-            }}
-          />
-          <TextInput
-            radius="md"
-            mb={"md"}
-            label="Title(Optional)"
-            description="Enter Your Title"
-            placeholder="Enter Your Title"
-            onChange={(e) => {
-              setdata({ ...data, title: e.target.value });
-            }}
-          />
-          <TextInput
-            type="date"
-            radius="md"
-            label="Expiry Date(Optional)"
-            placeholder="Enter Your Title"
-            onChange={(e) => {
-              setdata({ ...data, expiresAt: e.target.value });
-            }}
-          />
-          <Button
-            disabled={data?.originalUrl?.length > 10 ? false : true}
-            variant="outline"
-            color="red"
-            mt={"md"}
-            onClick={() => {
-              generateShortUrl(data);
-            }}
-          >
-            Generate Shortend Url
-          </Button>
-        </Container>
-      }
-    </div>
-  );
+   const [data, setData] = useState({
+       "originalUrl": "",
+       "expiresAt": "",
+       "title": "",
+       "customUrl": ""
+   })
+
+
+      return (
+         <Container size={"xs"}>
+             <TextInput
+                 mt={"xl"}
+                 withAsterisk
+                 size="lg"
+                 radius="lg"
+                 label="Original URL"
+                 placeholder="Enter URL to be shortened"
+                 onChange={(e)=>{
+                         setData({...data, originalUrl : e.target.value})
+                 }}
+             />
+              <TextInput
+                 size="lg"
+                 radius="lg"
+                 label="Custom Url (Optional)"
+                 placeholder="Enter Custom URL"
+                 onChange={(e)=>{
+                         setData({...data, customUrl : e.target.value})
+                 }}
+             />
+             <TextInput
+                 size="lg"
+                 radius="lg"
+                 label="Title (Optional)"
+                 placeholder="Enter Title for the URL"
+                 onChange={(e)=>{
+                         setData({...data, title : e.target.value})
+                 }}
+             />
+             <TextInput
+                 type='date'
+                 size="lg"
+                 radius="lg"
+                 label="Expiry Date (Optional)"
+                 placeholder="Choose Expiry Date"
+                 onChange={(e)=>{
+                         setData({...data, expiresAt : e.target.value})
+                 }}
+             />
+
+
+             <Button
+             onClick={()=>{
+               // console.log(data);
+               generateShortUrl(data);
+             }}
+             disabled = {data?.originalUrl?.length>10?false:true}
+             my="md" color={"red"}> Generate Short Url </Button>
+ 
+         </Container>
+     )
 }
--- frontend/src/Pages/UrlResponse.jsx
+++ frontend/src/Pages/UrlResponse.jsx
@@ -1,45 +1,53 @@
-import { Button, Container, CopyButton, Text, Textarea } from "@mantine/core";
+import { Button, Container, CopyButton, Text } from "@mantine/core";
+import { QRCodeSVG } from "qrcode.react";
 import Service from "../utils/http";
-import { QRCodeSVG } from "qrcode.react"
+
 const obj = new Service();
 
 export default function UrlResponse(props) {
   const surl = obj.getBaseURL() + "/api/s/" + props?.response?.shortCode;
+
   return (
     <div>
-      {
-        <Container size="xs" my={40} style={{ textAlign: "center" }}>
-          <Text
-            size="xl"
-            fw={900}
-            variant="gradient"
-            gradient={{ from: "grape", to: "grape", deg: 360 }}
-          >
-            Shortned URL:
-          </Text>
-          <Container bg={"blue.0"} p={10} mt={20} radius="md">
-            <Text size="md" radius="lg">
-              {surl}
-            </Text>
-          </Container>
-          <br />
-          <CopyButton mt="xl" value={surl}>
-            {({ copied, copy }) => (
-              <Button color={copied ? "teal" : "blue"} onClick={copy}>
-                {copied ? "Copied url" : "Copy url"}
-              </Button>
-            )}
-          </CopyButton>
-          <QRCodeSVG imageSettings={{
-                     excavate: true,
-                     src: '/HomeBackground.png',
-                     height: 100,
-                     width: 100
-                 }} value={surl} size={400}>
-                   <Image src={'/HomeBackground.png'} />
-           </QRCodeSVG>
+      <Container size="xs" my={40} style={{ textAlign: "center" }}>
+        <Text
+          size="xl"
+          fw={900}
+          variant="gradient"
+          gradient={{ from: "black", to: "grape", deg: 360 }}
+        >
+          Shortened URL:
+        </Text>
+
+        <Container bg={"blue.0"} p={10} mt={20} radius="md">
+          <Text size="md">{surl}</Text>
         </Container>
-      }
+
+        <br />
+
+        <CopyButton mt="md" value={surl}>
+          {({ copied, copy }) => (
+            <Button color={copied ? "teal" : "blue"} onClick={copy}>
+              {copied ? "Copied url" : "Copy url"}
+            </Button>
+          )}
+        </CopyButton>
+
+        <br />
+        <br />
+
+        {/* QR Code with embedded logo */}
+        <QRCodeSVG
+          imageSettings={{
+            excavate: true,
+            src: "/HomeBackground.png",
+            height: 100,
+            width: 100,
+          }}
+          value={surl}
+          size={400}
+        />
+      </Container>
     </div>
   );
 }
